51e6826cd02c56dd1d942b9f924560941464b26e
Awadhesh Gupta <awadheshg798@gmail.com>
2025-05-20 16:00:10 +0530

    Create Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Header from "./Components/Header/Header";
+import Services from "./Components/Services/Services";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Header />
+      <Services />
     </>
   );
 };
---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,24 @@
+import "./Services.css";
+import { services } from "../../data";
+
+const Services = () => {
+  return (
+    <section id="services">
+      <div className="overlay">
+        <div className="section-container">
+          {services.map((service, key) => (
+            <div className="service" key={key}>
+              <div className="flex-center icon">
+                <service.icon />
+              </div>
+              <h3 className="title">{service.title}</h3>
+              <p className="text-muted">{service.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
